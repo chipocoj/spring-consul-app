5b8760ddc169f76ed032e02d54d96fff548dda00
John Chipoco <johnc@magenic.com>
2022-01-11 00:20:17 -0600

    initial commit

---
+++ src/main/java/com/example/spring_consul_app/SpringConsulAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.spring_consul_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConsulAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConsulAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_consul_app/SpringConsulAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_consul_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConsulAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
